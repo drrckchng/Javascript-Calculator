1a4bff5c9baa87ee63bf36b73f27b30b18003d50
drrckchng <derrickchungx39@gmail.com>
2022-08-09 16:20:57 +0900

    Add feature to allow input of decimal. Currently, the rounding produces incorrect answer

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// Initial action plan...
-// On each click of button, push() value into array
-// Add input verification (must start with num, cant end with operator)
-// Upon clicking on "=" will call upon calculate() function
-//     should clear array and push() total value as first element of array
-// Add a backspace button that will pop() last element
-
 // Things to keep in mind...
-// User input validity. Need to make sure that users will press "number, operator, number"
 // Implement alert with a universal error (red screen/screen shake/err msg)
-// Additionally: need to verify user has inputed 2 numbers and operator before pressing "="
 
-// For future implementation
-// if user clicks on "decimal" button...
-// ensure the last value inputted into array is a numkey
-// if numkey, remove last element and add decimal and push() again to array
 
 function add(a, b) {
     return a + b;
@@ -64,11 +51,7 @@ function calculate(inputArray) {
 // is an operator or number or ETC function (plusminus, clear)
 // Will need to refactor function after...
 function inputVerify(button) {
-    console.log(button.target.classList.value);
-    console.log(button.target.value);
-
     if(button.target.classList.value === 'numkey') {
-
         if(checkExpressionNumPos()) {
             expression.push(button.target.value);
         // Prevent user from adding numbers onto stored result
@@ -77,35 +60,28 @@ function inputVerify(button) {
         } else {
             expression.push(expression.pop() + button.target.value.toString());
         }
-
     } else if(button.target.classList.value === 'operator') {
-
         if(!checkExpressionNumPos()) {
             expression.push(button.target.value);
             memoryStoreState = false;
         } else {
             alert("ERROR: Please enter a number");
         }
-
     } else if(button.target.value === "calculate") {
-
         if(!checkExpressionNumPos() && expression.length > 0) {
             memoryStoreState = true; // mark current status as storing mem
             console.log(calculate(expression));
         } else {
             alert("ERROR");
         }
-    
     // Clear last element of expression index
     } else if(button.target.value === "clear") {
         expression.pop();
-
+    // Clear all elements of expression
     } else if(button.target.value === "all-clear") {
         expression = [];
         memoryStoreState = false;
-
     }
-
 }
 
 // Check if expression array is 
