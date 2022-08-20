e2f088759d2b8bd34ffa6f8c573fc5a6cd1a5cfa
drrckchng <derrickchungx39@gmail.com>
2022-08-20 23:55:37 +0900

    Round final answer to 2 decimal places

--- script.js
+++ script.js
@@ -34,17 +34,22 @@ function operate(a, operator, b) {
 }
 
 // Will take tuples of the expression array and call operate() while keeping 
-// track of the total
+// track of the total 
 function calculate(inputArray) {
     let total = 0;
     while(inputArray.length >= 3) {
-        total = operate(inputArray[0], inputArray[1], inputArray[2]);
+        total = roundToTwo(operate(inputArray[0], inputArray[1], inputArray[2]));
         // remove first 3 elements
         inputArray.splice(0, 3);
         // add total so far to the front of array
         inputArray.unshift(total);
     }
     return total;
+}
+
+// Pulled from here https://stackoverflow.com/a/18358056
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
 }
 
 // Take the button that has been clicked and determine whether the button
@@ -57,38 +62,53 @@ function inputVerify(button) {
 
         if(checkExpressionNumPos()) {
             expression.push(button.target.value);
+
         // Prevent user from adding numbers onto stored result
         } else if(!checkExpressionNumPos() && memoryStoreState === true) {
             alert("ERROR: Please enter operator");
+
         // Prevent user from entering more than one decimal
         } else if (
             lastElement.charAt(lastElement.length - 1) == "." &&
             button.target.value === "."
         ){
             alert("ERROR: You can only enter one decimal");
+
         // Prevent user from entering more than one decimal PLACE
         } else if (lastElement.split(".")[1] > 1) {
             alert("ERROR: You can only enter one decimal place");
+
         } else {
-            expression.push(expression.pop() + button.target.value.toString());
+            expression.push(
+                expression.pop() + button.target.value.toString());
         }
+
     } else if(button.target.classList.value === 'operator') {
+
         if(!checkExpressionNumPos()) {
             expression.push(button.target.value);
             memoryStoreState = false;
+
         } else {
             alert("ERROR: Please enter a number");
+
         }
+
     } else if(button.target.value === "calculate") {
+
         if(!checkExpressionNumPos() && expression.length > 0) {
             memoryStoreState = true; // mark current status as storing mem
             console.log(calculate(expression));
+
         } else {
             alert("ERROR");
+
         }
+
     // Clear last element of expression index
     } else if(button.target.value === "clear") {
         expression.pop();
+
     // Clear all elements of expression
     } else if(button.target.value === "all-clear") {
         expression = [];
